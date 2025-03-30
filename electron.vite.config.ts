d85828d929aacc873ce7dc021938a3e38bdecbec
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-03-30 14:00:07 +0200

    ESM

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -4,12 +4,29 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   main: {
+    build: {
+      lib: {
+        entry: resolve(__dirname, 'src/main/index.ts'),
+        formats: ['es']
+      },
+      // target: 'node*'
+    },
     plugins: [externalizeDepsPlugin()]
   },
   preload: {
+    build: {
+      lib: {
+        entry: resolve(__dirname, 'src/preload/index.ts'),
+        formats: ['es']
+      },
+      // target: 'node*'
+    },
     plugins: [externalizeDepsPlugin()]
   },
   renderer: {
+    // build: {
+    //   target: 'chrome*'
+    // },
     resolve: {
       alias: {
         '@renderer': resolve('src/renderer/src')
